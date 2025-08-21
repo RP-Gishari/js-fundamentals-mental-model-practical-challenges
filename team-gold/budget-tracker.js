6dbc7d42f57ef5f2a3a5f8da17c02e792a56a13a
MUCYO Fred <mucyofred00@gmail.com>
2025-08-21 20:58:20 +0200

    added addExpense function

--- team-gold/budget-tracker.js
+++ team-gold/budget-tracker.js
@@ -21,3 +21,20 @@ const expense = [
     date: '2024-08-21',
   },
 ];
+
+function addExpense() {
+  let total = expense[0].amount + expense[1].amount + expense[2].amount;
+
+  console.log(`This is the total expense amount $${total}`);
+
+  return function calculateRemaining() {
+    const remaining = monthlyBudget - total;
+    if (remaining > 0) {
+      console.log(`Your remaining is $${remaining}`);
+    } else {
+      console.log('You are out of budget');
+    }
+  };
+}
+const exp = addExpense();
+exp();
