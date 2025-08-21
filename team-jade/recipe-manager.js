04982c131167b85db649589174eaf3208a031d13
Vainqueur123 <ndamukundavainqueur@gmail.com>
2025-08-21 18:18:41 +0200

    recommit

--- team-jade/recipe-manager.js
+++ team-jade/recipe-manager.js
@@ -2,9 +2,6 @@
 // 1. VARIABLES & SCOPE: Manage recipe data
 let recipeCollection = [];
 const measurementUnits = ['cups', 'tbsp', 'tsp', 'oz'];
-// Challenge: Keep recipe data organized and accessible
-
-// 2. FUNCTIONS: Recipe operations
 function addRecipe(name, ingredients, instructions, servings) {
 
   let addedRecipes = {
@@ -23,10 +20,9 @@ console.log(JSON.stringify(addRecipe(
     "Ugali", [{name:'cassava', amount: 56, unit: 'tbsp'}, {name: 'wheat', amount: 43, unit: "oz"}], 
     ['boil water ata 100%', 'prepare to mix it well'], 43
 ), null, 2));
-function scaleRecipe(recipeName, newServings) {}
-function searchRecipes(ingredient) {}
 
-// 3. OBJECTS: Recipe structure
+
+
 const recipe = {
     name: 'Chocolate Chip Cookies',
     ingredients: [
